298096b97a67ba5e721bd96e845cb75470b0b842
vishnurahar1 <vishnurahar1@gmail.com>
2022-10-27 00:27:52 +0530

    added btc/eth button on home page

--- app/src/main/java/com/example/cryptocheck/MainActivity.kt
+++ app/src/main/java/com/example/cryptocheck/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.cryptocheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cryptocheck.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.btcTv.setOnClickListener {
+            // open QR code scanner
+        }
+
+        binding.ethTv.setOnClickListener {
+            // open QR code scanner
+        }
     }
 }
